e27784b18b333dc0abcd39282922fbd14f6675a2
amanpandey-03 <amanpandeyjsr307@gmail.com>
2023-01-23 20:00:33 +0530

    initialised

---
+++ index.js
@@ -0,0 +1,14 @@
+// // document.getElementById("count").innerText=5
+// let count = 0
+// console.log(count)
+// // to print a value or to send it to console , 
+// // it must be initialised first 
+// let myAge=21
+
+
+let counter=0
+function increment() {
+    counter=counter+1
+    document.getElementById("count").innerText=counter
+console.log()
+}
