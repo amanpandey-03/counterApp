9e04edeae66a8093441f0e0700761c0aba436acd
amanpandey-03 <amanpandeyjsr307@gmail.com>
2023-01-24 03:17:46 +0530

    error in last step

--- index.js
+++ index.js
@@ -4,11 +4,29 @@
 // // to print a value or to send it to console , 
 // // it must be initialised first 
 // let myAge=21
+let b 
 
-
-let counter=0
+let countEl=document.getElementById("count")
+let count = 0
 function increment() {
-    counter=counter+1
-    document.getElementById("count").innerText=counter
-console.log()
+    count=count+1
+    countEl.innerText=count
+}
+
+function save(){
+  let  saveCount = counter
+  console.log(saveCount)
+  b=saveCount
+}
+
+function reset() {
+    count=0
+    countEl.innerText=count
+}
+
+
+function prev() {
+    let updateEnt = "Previous entries" 
+    let updateX = updateEnt + "   " +b
+    document.getElementById("prev").innerText = updateX
 }
